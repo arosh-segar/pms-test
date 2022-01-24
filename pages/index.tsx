7c258d1d646fc23fd5983b70b22b7e9053b1ef48
arosh-segar <arosh.segar@gmail.com>
2022-01-24 11:01:29 +0530

    google search console

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,10 @@ const Home: NextPage = () => {
       <Head>
         <title>pms front-end</title>
         <meta name="keywords" content="web" />
+        <meta
+          name="google-site-verification"
+          content="IrxsxGQvCM7G6VNMI5uMdto6fcaJo_jkaB5FHba3NpU"
+        />
       </Head>
       <h1 className="font-bold">Hello</h1>
     </div>
